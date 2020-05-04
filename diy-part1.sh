4c63040c84daf9ff0c4849ffdbc62c26258271c6
cq520 <48248970+cq520@users.noreply.github.com>
2020-05-04 20:09:55 +0800

    3
    3

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,4 @@ mkdir package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
-sed -i 's/'192.168.1.1'/'10.10.1.1'/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/'192.168.$((addr_offset++)).1'/'10.10.$((addr_offset++)).1'/g' openwrt/package/base-files/files/bin/config_generate
+
